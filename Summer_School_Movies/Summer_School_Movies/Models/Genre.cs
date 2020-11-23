2935c438277bdcda48147ea5cd49a04557c1b024
Lara Baker <52958722+larabaker96@users.noreply.github.com>
2020-11-23 16:23:27 +0200

    Changed to more basic model

--- Summer_School_Movies/Summer_School_Movies/Models/Genre.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace Summer_School_Movies.Models
-{
-    public class Genre
-    {
-        public int genreId { get; set; }
-        public string genreName;
-    }
-}
--- Summer_School_Movies/Summer_School_Movies/Models/Movie.cs
+++ Summer_School_Movies/Summer_School_Movies/Models/Movie.cs
@@ -11,17 +11,19 @@ namespace Summer_School_Movies.Models
 
         public string description { get; set; }
 
+        public string movieGenre { get; set; }
+
         [DataType(DataType.Date)]
         public string releaseDate { get; set; }
 
         public string ageRestriction { get; set; }
 
-        public Actor topActor1 { get; set; }
+        public string topActor1 { get; set; }
 
-        public Actor topActor2 { get; set; }
+        public string topActor2 { get; set; }
 
-        public Actor topActor3 { get; set; }
+        public string topActor3 { get; set; }
 
-        public Genre movieGenre { get; set; }
+        
     }
 }
