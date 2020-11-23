cb1f4391a919444118c9df3b3763ab7e5d99a64e
Lara Baker <52958722+larabaker96@users.noreply.github.com>
2020-11-23 16:43:36 +0200

    Added DbContext and Attempted to Scaffold
    Having a problem with scaffolding the controller, no code generator found.

---
+++ Summer_School_Movies/Summer_School_Movies/Models/MovieContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Summer_School_Movies.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
--- Summer_School_Movies/Summer_School_Movies/Startup.cs
+++ Summer_School_Movies/Summer_School_Movies/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Summer_School_Movies.Models;
 
 namespace Summer_School_Movies
 {
@@ -25,6 +27,7 @@ namespace Summer_School_Movies
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MovieContext>(opt => opt.UseInMemoryDatabase("MovieList"));
             services.AddControllers();
         }
 
